ee50209ea85eaff75e0496bccd6d2858210a015a
Nanway <nanway.chen@hotmail.com>
2020-09-25 18:27:47 -0700

    removed actually hehexd

--- build/cpp_localiser/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rsa/catkin_ws/src/cpp_localiser
-# Build directory: /home/rsa/catkin_ws/build/cpp_localiser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
